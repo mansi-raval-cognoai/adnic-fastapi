16dd974e8b677e9cda4b71e76abf8dbfa3eb32d6
mansi-raval-cognoai <mansi.raval@exotel.com>
2025-05-07 11:25:25 +0530

    Update get-user-details.py

--- get-user-details.py
+++ get-user-details.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Path to your CSV file
 CSV_FILE_PATH = 'ADNIC_Dummy_Customer_Data.csv'
 
-# Function to read CSV and return data as a dictionary using 'phonenumber' as key
+# Function to read CSV and return data as a dictionary using 'mobile_number' as key
 def read_csv_as_dict():
     data_dict = {}
     try:
@@ -26,9 +26,24 @@ def read_csv_as_dict():
 
 # Endpoint to return the CSV data
 @app.get("/data")
-async def get_csv_data(phonenumber: str):
+async def get_csv_data(mobile_number: str):
     data = read_csv_as_dict()
-    if isinstance(data, dict) and phonenumber in data:
-        return JSONResponse(content={"payload": data[phonenumber]})
+    customer_data = data.get(mobile_number)
+
+    if customer_data:
+        customer_name = customer_data["Customer Name"]
+        greeting = (
+            f"Hello {customer_name}! I am an ADNIC bot and I am here to assist you. I can see you have {customer_data['Policy Type']} policy and it has a validity from {customer_data['Validity']}. How may I assist you?"
+        )
+        return JSONResponse(content={
+            "payload": customer_data,
+            "greeting_message": greeting
+        })
     else:
-        return JSONResponse(content=data, status_code=400)
+        greeting = (
+            "Hello there! Welocme to ADNIC. I couldn’t find any active policy linked to your number. How may I assist you?"
+        )
+        return JSONResponse(content={
+            "payload": {},
+            "greeting_message": greeting
+        })
